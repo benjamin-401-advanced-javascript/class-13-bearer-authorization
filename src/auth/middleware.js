e0ac80b401c7bd14bf6f3bfc20320983f366cf81
Benjamin Clark <benisin@gmail.com>
2019-08-28 15:07:53 -0700

    added TOKEN_LIFE to user-model.

--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -3,36 +3,45 @@
 const User = require('./users-model.js');
 
 module.exports = (req, res, next) => {
-  
+
   try {
     let [authType, authString] = req.headers.authorization.split(/\s+/);
-    
-    switch( authType.toLowerCase() ) {
-      case 'basic': 
+
+    switch (authType.toLowerCase()) {
+      case 'basic':
         return _authBasic(authString);
-      default: 
+      case 'bearer':
+        return _authBearer(authString);
+      default:
         return _authError();
     }
   }
-  catch(e) {
+  catch (e) {
     next(e);
   }
-  
-  
+
+
   function _authBasic(str) {
     // str: am9objpqb2hubnk=
     let base64Buffer = Buffer.from(str, 'base64'); // <Buffer 01 02 ...>
     let bufferString = base64Buffer.toString();    // john:mysecret
     let [username, password] = bufferString.split(':'); // john='john'; mysecret='mysecret']
-    let auth = {username,password}; // { username:'john', password:'mysecret' }
-    
+    let auth = { username, password }; // { username:'john', password:'mysecret' }
+
     return User.authenticateBasic(auth)
-      .then(user => _authenticate(user) )
+      .then(user => _authenticate(user))
+      .catch(next);
+  }
+
+  function _authBearer(authToken) {
+    return User.authenticateToken(authToken)
+      .then(user => _authenticate(user))
       .catch(next);
   }
 
   function _authenticate(user) {
-    if(user) {
+    // add user and token to req if user exists
+    if (user) {
       req.user = user;
       req.token = user.generateToken();
       next();
@@ -41,9 +50,9 @@ module.exports = (req, res, next) => {
       _authError();
     }
   }
-  
+
   function _authError() {
     next('Invalid User ID/Password');
   }
-  
+
 };
